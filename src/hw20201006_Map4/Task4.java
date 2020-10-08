78ff8a3ef136fb5889089b56c8b5bb0d357bef9f
pointMS <m.sekacheva@gmail.com>
2020-10-08 16:19:11 +0200

    Task4 Map changed

--- src/hw20201006_Map4/Task4.java
+++ src/hw20201006_Map4/Task4.java
@@ -10,7 +10,9 @@ import java.util.Map;
 public class Task4 {
     public static void main(String[] args) {
         List<String> list = Arrays.asList("Ivan", "Peter", "Nick", "Nick", "Anna", "Anna", "Anna");
-        System.out.println(getNumber(list));
+        //System.out.println(getNumber(list));
+
+        System.out.println(getNumber2(list, "Nick"));
     }
 
     public static Map<String, Integer> getNumber(List<String> list) {
@@ -22,6 +24,24 @@ public class Task4 {
                 } else result.put(s, result.get(s) + 1);
             }
         }
+        return result;
+    }
+
+    //принимает строку, возвращает число
+    public static int getNumber2(List<String> list, String name){
+        int result = 0;
+        Map<String, Integer> map = new HashMap<>();
+        if (!list.isEmpty()) {
+            for (String s : list) {
+                if (!map.containsKey(s)) {
+                    map.put(s, 1);
+                } else map.put(s, map.get(s) + 1);
+                if (s.equals(name)) {
+                    result ++;
+                }
+            }
+        }
+        System.out.println("Name is : " + name);
         return result;
     }
 }
