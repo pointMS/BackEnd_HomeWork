69091b934ac7e1cf04318e63627868ba1efa38db
pointMS <m.sekacheva@gmail.com>
2020-10-14 15:08:45 +0200

    Words transformer #6

--- src/cw1013_WordTransformer/Main.java
+++ src/cw1013_WordTransformer/Main.java
@@ -1,28 +1,42 @@
 package cw1013_WordTransformer;
 
-import java.util.function.UnaryOperator;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class Main {
     //"abc defG ikl" -> "ABC defG IKL"
     // length == 3 -> toUpperCase
-
-    //Choose functional interfaces and extend method signature
-    //Adjust logic in the method to use these functional interfaces
+    // length == 5 -> *****
+    // length == 2 -> toLowerCase
 
     public static void main(String[] args) {
-        UnaryOperator<String> s = (s1) -> transform(s1);
-        System.out.println(s.apply("abc defG ikl"));
+
+        System.out.println(transform2("AB defG IKL"));
+        System.out.println(transform3("abc defG ikl"));
+        System.out.println(transform5("abcde defG iklmn"));
+
     }
 
-    public static String transform(String s) {
+    public static String transform(String s, Predicate<String> p, Function<String, String> f) {
         String[] temp = s.split(" ");
-        String result = "";
         for (int i = 0; i < temp.length; i++) {
-            if (temp[i].length() == 3) {
-                temp[i] = temp[i].toUpperCase();
+            if (p.test(temp[i])) {
+                temp[i] = f.apply(temp[i]);
             }
-            result = result.concat(" ").concat(temp[i]);
         }
-        return result;
+        return String.join(" ", temp);
+    }
+
+    public static String transform2(String s) {
+        return transform(s, (str) -> str.length() == 2, String::toLowerCase);
+    }
+
+    public static String transform3(String s) {
+        return transform(s, (str) -> str.length() == 3, String::toUpperCase);
+    }
+
+    public static String transform5(String s) {
+        return transform(s, str -> str.length() == 5, str -> str.replaceAll("[^*]", "*"));
     }
 }
